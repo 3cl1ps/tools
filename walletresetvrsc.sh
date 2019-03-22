9b950992298bf16806fa03dd7a466ed6e19786bc
3cl1ps <lagane.thomas@gmail.com>
2019-03-22 13:11:08 +0100

    Update walletresetvrsc.sh

--- walletresetvrsc.sh
+++ walletresetvrsc.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 cd "${BASH_SOURCE%/*}" || exit
 
-pubkey=$(cat pubkey.txt)
-
 coin="VRSC"
-daemon="${HOME}/VerusCoin/src/verusd -pubkey=${pubkey}"
+daemon="${HOME}/VerusCoin/src/verusd -pubkey=$PUBKEY"
 daemon_process_regex="verusd.*\-pubkey"
 cli="komodo-cli -ac_name=VRSC"
 wallet_file="${HOME}/.komodo/VRSC/wallet.dat"
-nn_address="RQipE6ycbVVb9vCkhqrK8PGZs2p5YmiBtg"
+nn_address=$KMDADDRESS
 
 ./walletreset.sh \
   "${coin}" \
