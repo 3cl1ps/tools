9e95a405eb12a0d2ac182b3c2ff8b1af938edf1d
3cl1ps <lagane.thomas@gmail.com>
2019-06-03 10:06:19 +0200

    Update walletresetvrsc.sh

--- walletresetvrsc.sh
+++ walletresetvrsc.sh
@@ -2,7 +2,7 @@
 cd "${BASH_SOURCE%/*}" || exit
 
 coin="VRSC"
-daemon="${HOME}/VerusCoin/src/verusd -pubkey=$PUBKEY"
+daemon="/home/eclips/VerusCoin/src/komodod -ac_name=VRSC -ac_algo=verushash -ac_cc=1 -ac_supply=0 -ac_eras=3 -ac_reward=0,38400000000,2400000000 -ac_halving=1,43200,1051920 -ac_decay=100000000,0,0 -ac_end=10080,226080,0 -ac_timelockgte=19200000000 -ac_timeunlockfrom=129600 -ac_timeunlockto=1180800 -ac_veruspos=50 -addnode=95.216.13.66"
 daemon_process_regex="verusd.*\-pubkey"
 cli="komodo-cli -ac_name=VRSC"
 wallet_file="${HOME}/.komodo/VRSC/wallet.dat"
