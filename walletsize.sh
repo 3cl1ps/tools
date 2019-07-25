ef7d1d591100ac627405ed3801096b7e99f022e0
3cl1ps <lagane.thomas@gmail.com>
2019-07-25 22:21:06 +0200

    Update walletsize.sh

--- walletsize.sh
+++ walletsize.sh
@@ -14,7 +14,7 @@ CYAN="\033[36m"
 WHITE="\033[37m"
 
 function show_walletsize () {
-  if [ "$1" != "KMD" ] && [ "$1" != "BTC" ] && [ "$1" != "EMC2" ] && [ "$1" != "GAME" ] && [ "$1" != "HUSH" ] && [ "$1" != "CHIPS" ] && [ "$1" != "GIN" ]; then
+  if [ "$1" != "KMD" ] && [ "$1" != "BTC" ] then
     if [ -f ~/.komodo/$1/wallet.dat ]; then
       SIZE=$(stat ~/.komodo/$1/wallet.dat | grep -Po "\d+" | head -1)
     else
@@ -24,14 +24,6 @@ function show_walletsize () {
     SIZE=$(stat /bitcoin/wallet.dat | grep -Po "\d+" | head -1)
   elif [ "$1" = "KMD" ]; then
     SIZE=$(stat ~/.komodo/wallet.dat | grep -Po "\d+" | head -1)
-  elif [ "$1" = "EMC2" ]; then
-    SIZE=$(stat ~/.einsteinium/wallet.dat | grep -Po "\d+" | head -1)
-  elif [ "$1" = "GAME" ]; then
-    SIZE=$(stat ~/.gamecredits/wallet.dat | grep -Po "\d+" | head -1)
-  elif [ "$1" = "CHIPS" ]; then
-    SIZE=$(stat ~/.chips/wallet.dat | grep -Po "\d+" | head -1)
-  elif [ "$1" = "GIN" ]; then
-    SIZE=$(stat ~/.gincoincore/wallet.dat | grep -Po "\d+" | head -1)
   fi
 
   OUTSTR=$(echo $SIZE | numfmt --to=si --suffix=B)
@@ -53,10 +45,6 @@ ignore_list=(
 
 show_walletsize KMD
 show_walletsize BTC
-show_walletsize EMC2
-show_walletsize GAME
-show_walletsize CHIPS
-show_walletsize GIN
 
 # Only assetchains
 ${HOME}/komodo/src/listassetchains | while read list; do
