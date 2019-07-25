db33311c8432f6cda2394890e7f44153ae4a46f5
3cl1ps <lagane.thomas@gmail.com>
2019-07-25 22:28:18 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -2,10 +2,9 @@
 # Suggest using with this command: watch --color -n 60 ./status
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
-#printf "Notary Node Status\n"
-#printf "==================\n"
+printf "Notary Node Status\n"
+printf "==================\n"
 
 function process_check () {
   ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
@@ -21,7 +20,7 @@ fi
 
 UP="$(/usr/bin/uptime)"
 
-#echo "Server Uptime: $UP"
+echo "Server Uptime: $UP"
 #TO DO
 #ADD UPTIME CHECK
 #ADD LOW BALANCE CHECK
@@ -42,7 +41,6 @@ processlist=(
 'MSHARK'
 'BOTS'
 'MGW'
-#'COQUI'
 'WLC'
 'KV'
 'CEAL'
@@ -60,6 +58,7 @@ processlist=(
 'EQL'
 'ZILLA'
 'RFOX'
+'HUSH3'
 'SEC'
 'CCL'
 'PIRATE'
@@ -71,11 +70,9 @@ processlist=(
 'ILN'
 'RICK'
 'MORTY'
-'HUSH3'
 'KOIN'
 'ZEXO'
 'K64'
-#'COQUICASH'
 'THC'
 )
 
@@ -96,28 +93,30 @@ do
     printf "Process: ${GREEN} Running ${NC}"
     if [ "$count" = "1" ]
     then
-            RESULT="$(komodo-cli -rpcclienttimeout=15 listunspent | grep .00010000 | wc -l)"
-            RESULT1="$(komodo-cli -rpcclienttimeout=15 listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
-            RESULT2="$(komodo-cli -rpcclienttimeout=15 getbalance)"
+            cd ~/komodo/src
+            RESULT="$(./komodo-cli -rpcclienttimeout=15 listunspent | grep .00010000 | wc -l)"
+            RESULT1="$(./komodo-cli -rpcclienttimeout=15  listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
+            RESULT2="$(./komodo-cli -rpcclienttimeout=15 getbalance)"
     fi
     if [ "$count" = "2" ]
     then
             RESULT="$(bitcoin-cli -rpcclienttimeout=15 listunspent | grep .00010000 | wc -l)"
-            RESULT1="$(bitcoin-cli -rpcclienttimeout=15 listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
+            RESULT1="$(bitcoin-cli -rpcclienttimeout=15  listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
             RESULT2="$(bitcoin-cli -rpcclienttimeout=15 getbalance)"
 
     fi
     if [ "$count" -gt "2" ]
     then
-            RESULT="$(komodo-cli -rpcclienttimeout=15 -ac_name=${processlist[count]} listunspent | grep .00010000 | wc -l)"
-            RESULT1="$(komodo-cli -ac_name=${processlist[count]} -rpcclienttimeout=15 listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
-            RESULT2="$(komodo-cli -rpcclienttimeout=15 -ac_name=${processlist[count]} getbalance)"
+            cd ~/komodo/src
+            RESULT="$(./komodo-cli -rpcclienttimeout=15 -ac_name=${processlist[count]} listunspent | grep .00010000 | wc -l)"
+            RESULT1="$(./komodo-cli -ac_name=${processlist[count]} -rpcclienttimeout=15  listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
+            RESULT2="$(./komodo-cli -rpcclienttimeout=15 -ac_name=${processlist[count]} getbalance)"
     fi
 # Check if we have actual results next two lines check for valid number.
     if [[ $RESULT == ?([-+])+([0-9])?(.*([0-9])) ]] ||
        [[ $RESULT == ?(?([-+])*([0-9])).+([0-9]) ]]
     then
-    if [ "$RESULT" -lt "10" ]
+    if [ "$RESULT" -lt "30" ]
     then
     printf  " - Avail UTXOs: ${RED}$RESULT\t${NC}"
     else
@@ -140,16 +139,15 @@ do
     if [[ $RESULT2 == ?([-+])+([0-9])?(.*([0-9])) ]] ||
        [[ $RESULT2 == ?(?([-+])*([0-9])).+([0-9]) ]]
     then
-    if (( $(echo "$RESULT2 > 1" | bc -l) ));
+    if (( $(echo "$RESULT2 > 0.1" | bc -l) ));
     then
     printf  " - Avail Funds: ${GREEN}$RESULT2\t${NC}\n"
+ #   printf "\t - Current Block: X\t - Longest Chain: X - Last Notarized: X\n"
 
-    else  if (( $(echo "$RESULT2 > 0.1" | bc -l) ));
-      then
-      printf  " - Avail Funds: ${YELLOW}$RESULT2\t${NC}\n"
-      else
-      printf  " - Avail Funds: ${RED}$RESULT2\t${NC}\n"
-      fi
+    else
+    printf  " - Avail Funds: ${RED}$RESULT2\t${NC}\n"
+#    printf "\t - Current Block: X\t - Longest Chain: X - Last Notarized: X\n"
+
     fi
     else
       printf "\n"
