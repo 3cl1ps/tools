0066109d35cfdae6c7c192f5a9ed15e86965a46d
3cl1ps <lagane.thomas@gmail.com>
2019-08-11 12:47:25 +0200

    Update listclis.sh

--- listclis.sh
+++ listclis.sh
@@ -6,34 +6,10 @@ cd "${BASH_SOURCE%/*}" || exit
 specific_coin=$1
 
 bitcoin_cli="bitcoin-cli"
-chips_cli="chips-cli"
-game_cli="gamecredits-cli"
-hush_cli="hush-cli"
-einsteinium_cli="einsteinium-cli"
-gincoin_cli="gincoin-cli"
 komodo_cli="komodo-cli"
-verus_cli="verus"
 
 if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "BTC" ]]; then
   echo ${bitcoin_cli}
-fi
-if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "CHIPS" ]]; then
-  echo ${chips_cli}
-fi
-if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "GAME" ]]; then
-  echo ${game_cli}
-fi
-if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "HUSH" ]]; then
-  echo ${hush_cli}
-fi
-if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "EMC2" ]]; then
-  echo ${einsteinium_cli}
-fi
-if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "GIN" ]]; then
-  echo ${gincoin_cli}
-fi
-if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "VRSC" ]]; then
-  echo ${verus_cli}
 fi
 if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "KMD" ]]; then
   echo ${komodo_cli}
