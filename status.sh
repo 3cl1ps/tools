b678a06288901e6882ca28ec93c242b4347913b9
3cl1ps <lagane.thomas@gmail.com>
2020-01-23 13:28:20 +0100

    up

--- status.sh
+++ status.sh
@@ -2,6 +2,13 @@
 # Suggest using with this command: watch --color -n 60 ./status
 scriptpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $scriptpath/main
+TIMEFORMAT=%R
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+btcntrzaddr=1P3rU1Nk1pmc2BiWC8dEy9bZa1ZbMp5jfg
+kmdntrzaddr=RXL3YXG2ceaB6C5hfJcN4fvmLH2C34knhA
 
 checkRepo () {
     if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
@@ -20,26 +27,10 @@ checkRepo () {
     cd $prevdir
 
     if [ $localrev != $remoterev ]; then
-        printf "$color_red[U]$color_reset"
+        printf "${RED}[U]${NC}"
     fi
-
-    case $1 in
-        KMD|GIN)
-            printf "     "
-            ;;
-        CHIPS)
-            printf "   "
-            ;;
-        GAME|EMC2)
-            printf "    "
-            ;;
-    esac
 }
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
 
 processlist=(
 #'iguana'
@@ -47,7 +38,7 @@ processlist=(
 'bitcoind'
 )
 
-echo -n -e "iguana \t\t"
+printf "%-13s" "iguana" 
 if ps aux | grep -v grep | grep iguana >/dev/null
 then 
     printf "${GREEN} Running ${NC}\n"
@@ -58,17 +49,8 @@ fi
 count=0
 while [ "x${processlist[count]}" != "x" ]
 do
-    echo -n "${processlist[count]}"
     repo=(${repos[KMD]})
-    echo -n "$(checkRepo KMD ${repo[0]} ${repo[1]})"
-    #fixes formating issues
-    size=${#processlist[count]}
-    if [ "$size" -lt "8" ]
-    then
-        echo -n -e "\t\t"
-    else
-        echo -n -e "\t"
-    fi
+    printf "%-9s %3s" ${processlist[count]} $(checkRepo KMD ${repo[0]} ${repo[1]})
     if ps aux | grep -v grep | grep ${processlist[count]} >/dev/null
     then
         printf "${GREEN} Running ${NC}"
@@ -78,12 +60,18 @@ do
             RESULT1="$(komodo-cli -rpcclienttimeout=15  listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
             RESULT2="$(komodo-cli -rpcclienttimeout=15 getbalance)"
             SIZE=$(stat --printf="%s" /home/eclips/.komodo/wallet.dat)
+            TIME=$((time komodo-cli listunspent) 2>&1 >/dev/null)
+            txinfo=$(komodo-cli listtransactions "" $txscanamount)
+            lastntrztime=$(echo $txinfo | jq -r --arg address "$kmdntrzaddr" '[.[] | select(.address==$address)] | sort_by(.time) | last | "\(.time)"') 
         fi
         if [ "$count" = "1" ]
         then
             RESULT="$(bitcoin-cli -rpcclienttimeout=15 listunspent | grep .00010000 | wc -l)"
             RESULT1="$(bitcoin-cli -rpcclienttimeout=15  listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
             RESULT2="$(bitcoin-cli -rpcclienttimeout=15 getbalance)"
+            txinfo=$(bitcoin-cli listtransactions "" $txscanamount)
+            lastntrztime=$(echo $txinfo | jq -r --arg address "$btcntrzaddr" '[.[] | select(.address==$address)] | sort_by(.time) | last | "\(.time)"')
+            
             if [ -e /home/eclips/.bitcoin/wallet.dat ]
             then
                 SIZE=$(stat --printf="%s" /home/eclips/.bitcoin/wallet.dat)
@@ -92,15 +80,16 @@ do
             then
                 SIZE=$(stat --printf="%s" /bitcoin/wallet.dat)
             fi
+            TIME=$((time bitcoin-cli listunspent) 2>&1 >/dev/null)
         fi
         # Check if we have actual results next two lines check for valid number.
         if [[ $RESULT == ?([-+])+([0-9])?(.*([0-9])) ]] || [[ $RESULT == ?(?([-+])*([0-9])).+([0-9]) ]]
         then
             if [ "$RESULT" -lt "30" ]
             then
-                printf  " - UTXOs: ${RED}$RESULT\t${NC}"
+                printf  " - UTXOs: ${RED}%3s${NC}" $RESULT
             else
-                printf  " - UTXOs: ${GREEN}$RESULT\t${NC}"
+                printf  " - UTXOs: ${GREEN}%3s${NC}" $RESULT
             fi
         fi
 
@@ -108,9 +97,9 @@ do
         then
             if [ "$RESULT1" -gt "0" ]
             then
-                printf  " - Dust: ${RED}$RESULT1\t${NC}"
+                printf  " - Dust: ${RED}%3s${NC}" $RESULT1
             else
-                printf  " - Dust: ${GREEN}$RESULT1\t${NC}"
+                printf  " - Dust: ${GREEN}%3s${NC}" $RESULT1
             fi
         fi
 
@@ -118,9 +107,9 @@ do
         then
             if (( $(echo "$RESULT2 > 0.1" | bc -l) ));
             then
-                printf  " - Funds: ${GREEN}$RESULT2\t${NC}"
+                printf " - Funds: ${GREEN}%9.2f${NC}" $RESULT2
             else
-                printf  " - Funds: ${RED}$RESULT2\t${NC}"
+                printf " - Funds: ${RED}%9.2f${NC}" $RESULT2
             fi
         else
             printf "\n"
@@ -128,13 +117,29 @@ do
 
         OUTSTR=$(echo $SIZE | numfmt --to=si --suffix=B)
         if [ "$SIZE" -gt "4000000" ]; then
-            printf " - ${RED}$OUTSTR${NC}\n"            
+            printf " - WSize: ${RED}%5s${NC}" $OUTSTR           
         else
-            printf " - ${GREEN}$OUTSTR${NC}\n"
+            printf " - WSize: ${GREEN}%5s${NC}" $OUTSTR
+        fi
+        
+        if [[ "$TIME" > "0.1" ]]; then
+            printf " - Time: ${RED}%3ss${NC}" $TIME          
+        else
+            printf " - Time: ${GREEN}%3ss${NC}" $TIME
         fi
 
+        #if [[ "$lastntrztime" > "0.1" ]]; then
+        #    printf " - ${RED}%3ss${NC}" $TIME          
+        #else
+        printf " - LastN: ${GREEN}%6s${NC}" $(timeSince $lastntrztime)
+        #fi
+
+        printf "\n"
         RESULT=""
+        RESULT1=""
         RESULT2=""
+        SIZE=""
+        TIME=""
 
     else
         printf "${RED} Not Running ${NC}\n"
@@ -148,15 +153,7 @@ if [[ "${ignoreacs[@]}" =~ "${list}" ]]; then
     continue
 fi
 
-echo -n "${list}"
-#fixes formating issues
-size=${#list}
-if [ "$size" -lt "8" ]
-then
-    echo -n -e "\t\t"
-else
-    echo -n -e "\t"
-fi
+printf "%-13s" "${list}"
 if ps aux | grep -v grep | grep ${list} >/dev/null
 then
     printf "${GREEN} Running ${NC}"
@@ -164,14 +161,17 @@ then
     RESULT1="$(komodo-cli -ac_name=${list} -rpcclienttimeout=15  listunspent|grep amount|awk '{print $2}'|sed s/.$//|awk '$1 < 0.0001'|wc -l)"
     RESULT2="$(komodo-cli -rpcclienttimeout=15 -ac_name=${list} getbalance)"
     SIZE=$(stat --printf="%s" ~/.komodo/${list}/wallet.dat)
+    TIME=$((time komodo-cli -ac_name=${list} listunspent) 2>&1 >/dev/null)
+    txinfo=$(komodo-cli -ac_name=${list} listtransactions "" $txscanamount)
+    lastntrztime=$(echo $txinfo | jq -r --arg address "$kmdntrzaddr" '[.[] | select(.address==$address)] | sort_by(.time) | last | "\(.time)"') 
     # Check if we have actual results next two lines check for valid number.
     if [[ $RESULT == ?([-+])+([0-9])?(.*([0-9])) ]] || [[ $RESULT == ?(?([-+])*([0-9])).+([0-9]) ]]
     then
         if [ "$RESULT" -lt "30" ]
         then
-            printf  " - UTXOs: ${RED}$RESULT\t${NC}"
+            printf  " - UTXOs: ${RED}%3s${NC}" $RESULT
         else
-            printf  " - UTXOs: ${GREEN}$RESULT\t${NC}"
+            printf  " - UTXOs: ${GREEN}%3s${NC}" $RESULT
         fi
     fi
 
@@ -179,9 +179,9 @@ then
     then
         if [ "$RESULT1" -gt "0" ]
         then
-            printf  " - Dust: ${RED}$RESULT1\t${NC}"
+            printf  " - Dust: ${RED}%3s${NC}" $RESULT1
         else
-            printf  " - Dust: ${GREEN}$RESULT1\t${NC}"
+            printf  " - Dust: ${GREEN}%3s${NC}" $RESULT1
         fi
     fi
 
@@ -189,28 +189,33 @@ then
     then
         if (( $(echo "$RESULT2 > 0.1" | bc -l) ));
         then
-            printf  " - Funds: ${GREEN}$RESULT2${NC}"
+            printf " - Funds: ${GREEN}%9.2f${NC}" $RESULT2
         else
-            printf  " - Funds: ${RED}$RESULT2${NC}"
+            printf " - Funds: ${RED}%9.2f${NC}" $RESULT2
         fi
-    fi
-    size=${#RESULT2}
-    if [ "$size" -gt "13" ]
-    then
-        echo -n -e ""
-    else
-        echo -n -e "\t"
     fi
 
     OUTSTR=$(echo $SIZE | numfmt --to=si --suffix=B)
     if [ "$SIZE" -gt "4000000" ]; then
-        printf " - ${RED}$OUTSTR${NC}\n" 
+        printf " - WSize: ${RED}%5s${NC}" $OUTSTR 
     else
-        printf " - ${GREEN}$OUTSTR${NC}\n"
+        printf " - WSize: ${GREEN}%5s${NC}" $OUTSTR
     fi
 
+    if [[ "$TIME" > "0.1" ]]; then
+        printf " - Time: ${RED}%5ss${NC}" $TIME          
+    else
+        printf " - Time: ${GREEN}%5ss${NC}" $TIME
+    fi
+
+    printf " - LastN: ${GREEN}%6s${NC}" $(timeSince $lastntrztime)
+
+    printf "\n"
     RESULT=""
+    RESULT1=""
     RESULT2=""
+    TIME=""
+    SIZE=""
 
 else
     printf "${RED} Not Running ${NC}\n"
