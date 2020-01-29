24c64ed8e01703d02bfc25e1279093018d50254d
3cl1ps <lagane.thomas@gmail.com>
2020-01-29 21:09:10 +0100

    Update consolidateutxos.sh

--- consolidateutxos.sh
+++ consolidateutxos.sh
@@ -13,7 +13,7 @@ coin="KMD"
 asset=""
 
 unspent=$(komodo-cli $asset listunspent)
-consolidateutxo=$(jq -r --arg checkaddr $nn_address '[.[] | select (.address==$checkaddr and .spendable==true)] | sort_by(-.amount)[0:399]' <<< $unspent)
+consolidateutxo=$(jq -r --arg checkaddr $KMDADDRESS '[.[] | select (.address==$checkaddr and .spendable==true)] | sort_by(-.amount)[0:399]' <<< $unspent)
 consolidatethese=$(jq -r '[.[] | {"txid":.txid, "vout":.vout}] | tostring' <<< $consolidateutxo)
 consolidateamount=$(jq -r '[.[].amount] | add' <<< $consolidateutxo)
 
@@ -22,7 +22,7 @@ if [[ "$consolidateamount" != "null" ]]; then
 
     if (( $(echo "$consolidateamountfixed > 0" | bc -l) )); then
 
-        rawtxresult=$(komodo-cli $asset createrawtransaction ${consolidatethese} '''{ "'$nn_address'": '$consolidateamountfixed' }''')
+        rawtxresult=$(komodo-cli $asset createrawtransaction ${consolidatethese} '''{ "'$KMDADDRESS'": '$consolidateamountfixed' }''')
         rawtxid=$(sendRaw ${rawtxresult} ${coin})
 
         echo "Sent $consolidateamount to $nn_address TXID: $rawtxid"
