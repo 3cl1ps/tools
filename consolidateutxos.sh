8951a0eb0127f41d80f3f9189bb74378bf127396
3cl1ps <lagane.thomas@gmail.com>
2020-01-29 21:05:47 +0100

    Update consolidateutxos.sh

--- consolidateutxos.sh
+++ consolidateutxos.sh
@@ -12,7 +12,7 @@ source /home/eclips/tools/main
 coin="KMD"
 asset=""
 
-unspent=$($komodocli $asset listunspent)
+unspent=$(komodo-cli $asset listunspent)
 consolidateutxo=$(jq -r --arg checkaddr $nn_address '[.[] | select (.address==$checkaddr and .spendable==true)] | sort_by(-.amount)[0:399]' <<< $unspent)
 consolidatethese=$(jq -r '[.[] | {"txid":.txid, "vout":.vout}] | tostring' <<< $consolidateutxo)
 consolidateamount=$(jq -r '[.[].amount] | add' <<< $consolidateutxo)
@@ -22,7 +22,7 @@ if [[ "$consolidateamount" != "null" ]]; then
 
     if (( $(echo "$consolidateamountfixed > 0" | bc -l) )); then
 
-        rawtxresult=$($komodocli $asset createrawtransaction ${consolidatethese} '''{ "'$nn_address'": '$consolidateamountfixed' }''')
+        rawtxresult=$(komodo-cli $asset createrawtransaction ${consolidatethese} '''{ "'$nn_address'": '$consolidateamountfixed' }''')
         rawtxid=$(sendRaw ${rawtxresult} ${coin})
 
         echo "Sent $consolidateamount to $nn_address TXID: $rawtxid"
