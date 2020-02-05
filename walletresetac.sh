5f38999c42d7cb271ea0e85c911dd267a1945556
3cl1ps <lagane.thomas@gmail.com>
2020-02-05 16:39:34 +0100

    up

--- walletresetac.sh
+++ walletresetac.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+#cd "${BASH_SOURCE%/*}" || exit
 source /home/eclips/tools/main
-cd "${BASH_SOURCE%/*}" || exit
-
 # Coin we're resetting
 coin=$1
 
-daemon="komodod $(./listassetchainparams ${coin}) -pubkey=$PUBKEY"
+daemon="komodod $(/home/eclips/tools/listassetchainparams ${coin}) -pubkey=$PUBKEY"
 daemon_process_regex="komodod.*\-ac_name=${coin} -"
 cli="komodo-cli -ac_name=${coin}"
 wallet_file="${HOME}/.komodo/${coin}/wallet.dat"
 
-./walletreset.sh \
+/home/eclips/tools/walletreset.sh \
   "${coin}" \
   "${daemon}" \
   "${daemon_process_regex}" \
