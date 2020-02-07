b146a8a38083b9077013cdcd92aa02038116fa5d
3cl1ps <lagane.thomas@gmail.com>
2020-02-07 11:06:35 +0100

    Update walletresetkmd.sh

--- walletresetkmd.sh
+++ walletresetkmd.sh
@@ -3,7 +3,7 @@ source /home/eclips/tools/main
 cd "${BASH_SOURCE%/*}" || exit
 
 coin="KMD"
-daemon="komodod -notary -gen -genproclimit=1 -pubkey=${PUBKEY}"
+daemon="/home/eclips/tools/komodod -notary -gen -genproclimit=1 -pubkey=${PUBKEY} -minrelaytxfee=0.000035 -opretmintxfee=0.004"
 daemon_process_regex="komodod.*\-notary"
 cli="komodo-cli"
 wallet_file="${HOME}/.komodo/wallet.dat"
