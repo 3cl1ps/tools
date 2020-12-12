475297e71aa2a102df8e8915b1966a17a5e299a8
3cl1ps <lagane.thomas@gmail.com>
2020-12-12 23:19:56 +0300

    toto

--- distrib.sh
+++ distrib.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 addr_ecl=RYFxQivEUrPor7Xmfu1dGECsKGSLewdtXa
-#addr_lud
-#addr_yassin
+addr_lud=RYFxQivEUrPor7Xmfu1dGECsKGSLewdtXa
+addr_yassin=RBTSQxYFx32sXN3A5js9PQsK4ZPGEzZ5Ep
+triggerPai=8
+balance=$(komodo-cli getbalance)
+echo $balance
 
 tour=0
 while true; do
     case $tour in
-
         0)
+            addr=$addr_yassin
+            while ! ./payday3 $addr; do  
+                sleep 1
+            done
             echo paiement yassin
-            #/home/eclips/tools/payday3 $addr_yassin
             ;;
-
         1)
+            addr=$addr_lud
+            while ! ./payday3 $addr; do  
+                sleep 1
+            done
             echo paiement lud
-            #/home/eclips/tools/payday3 $addr_lud
             ;;
-
         2)
-            echo paiement ecl
-            #/home/eclips/tools/payday3 $addr_ecl
+            addr=$addr_ecl
+            while ! ./payday3 $addr; do  
+                sleep 1
+            done
+            echo paiement eclips
             ;;
-
         *)
             echo error $tour
             ;;
     esac
     tour=$(($tour + 1))
     if [ $tour -eq 3 ]; then tour=0; echo reset; fi
-    sleep 72000 #20h
+    #sleep 72000 #20h
+    sleep 1
 done
